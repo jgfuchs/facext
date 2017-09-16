1a2e022ac16ebbe258c0b7cf5742fa6580de449b
Kyle Swanson <swansonk.14@gmail.com>
2017-09-16 15:18:18 -0400

    Working face detection and drawing on webcam on options page

--- options.js
+++ options.js
@@ -1,30 +1,34 @@
 window.onload = () => {
-	var video = document.getElementById('video');
+    var video = document.getElementById('video'),
+        video_width = video.width,
+        video_height = video.height,
+        overlay = document.getElementById('overlay'),
+        overlayCC =overlay.getContext('2d');
 
     navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(
         (stream) => {
-           console.log("Success: ", stream);
-           video.src = window.URL.createObjectURL(stream);
-           video.play();
+            console.log("Success: ", stream);
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
 
-           /*
+            var ctrack = new clm.tracker();
+            ctrack.init();
 
-           var ctrack = new clm.tracker();
-           ctrack.init();
+            ctrack.start(video);
 
-           ctrack.start(video);
+            function drawLoop() {
+                window.requestAnimationFrame(drawLoop);
+				overlayCC.clearRect(0, 0, video_width, video_height);
+				if (ctrack.getCurrentPosition()) {
+					ctrack.draw(overlay);
+				}
+            }
 
-           function drawLoop() {
-
-           }
-
-           requestAnimFrame(drawLoop);
-           */
-
+            drawLoop();
         }
     ).catch(
         (err) => {
-           console.log("Error: ", err);
+            console.log("Error: ", err);
         }
     )
 };
