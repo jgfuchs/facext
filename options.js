21a87f5ef0b2adf27fcc793095b875e354f763ec
Kyle Swanson <swansonk.14@gmail.com>
2017-09-16 14:53:43 -0400

    Webcam feed on options page (working)

--- options.js
+++ options.js
@@ -1,7 +1,11 @@
 window.onload = () => {
+	var video = document.getElementById('video');
+	
     navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(
         (stream) => {
            console.log("Success: ", stream);
+           video.src = window.URL.createObjectURL(stream);
+           video.play();
         }
     ).catch(
         (err) => {
