ae206afb787246459f7f8ec8e95d5ffd1a90ec0b
Julian Fuchs <jfuchs@mit.edu>
2017-09-16 14:55:34 -0400

    Improved options page

--- options.js
+++ options.js
@@ -1,11 +1,16 @@
+byid = (id) => document.getElementById(id);
+
 window.onload = () => {
     navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(
         (stream) => {
-           console.log("Success: ", stream);
+            byid("ok").style.display = "block";
+            byid("status").innerHTML = "You can now close this window.";
         }
     ).catch(
         (err) => {
-           console.log("Error: ", err);
+            byid("error").style.display = "block";
+            console.log(err);
+            byid("status").innerHTML = "Caught " + err.name;
         }
     )
 };
