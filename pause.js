e3b5904f6ca03704e165ba71c96e7281fbe11bd6
Kelsey Chan <kelseykchan@gmail.com>
2017-09-16 18:20:16 -0400

    Minify play/pause script

--- pause.js
+++ pause.js
@@ -1,9 +1,1 @@
-if (window.location.hostname == "www.youtube.com" && window.location.pathname == "/watch") {
-  var playPauseButton = document.getElementsByClassName('ytp-play-button ytp-button')[0];
-  if (playPauseButton.getAttribute('aria-label') == 'Pause') {
-      playPauseButton.click();
-  }
-} else if (window.location.hostname == "www.netflix.com" && window.location.pathname.startsWith("/watch")) {
-  var playPauseButton = document.getElementsByClassName('player-control-button player-play-pause pause')[0];
-  playPauseButton.click();
-}
+document.getElementsByTagName('video')[0].pause();
