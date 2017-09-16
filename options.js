387d09992786974b027e74bedeb0957b0c7fe617
Kyle Swanson <swansonk.14@gmail.com>
2017-09-16 14:58:20 -0400

    Begin tracking

--- options.js
+++ options.js
@@ -1,11 +1,26 @@
 window.onload = () => {
 	var video = document.getElementById('video');
-	
+
     navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(
         (stream) => {
            console.log("Success: ", stream);
            video.src = window.URL.createObjectURL(stream);
            video.play();
+
+           /*
+
+           var ctrack = new clm.tracker();
+           ctrack.init();
+
+           ctrack.start(video);
+
+           function drawLoop() {
+
+           }
+
+           requestAnimFrame(drawLoop);
+           */
+
         }
     ).catch(
         (err) => {
