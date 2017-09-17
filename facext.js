0d9f222f1f02aa166d0bddb03ab4b2aba9a10166
Kyle Swanson <swansonk.14@gmail.com>
2017-09-16 22:12:45 -0400

    charts partly working

--- facext.js
+++ facext.js
@@ -1,12 +1,72 @@
 window.onload = () => {
     var video = document.getElementById('video'),
         face_present = false,
-        emotion_classifier = new emotionClassifier();
+        emotion_classifier = new emotionClassifier(),
+        emotion_names = Object.keys(emotionModel),
+        emotion_data = {};
 
-    var emotion_data = {}
-    for (var emotion in emotionModel) {
+    emotion_names.forEach((emotion) => {
         emotion_data[emotion] = [];
-    }
+    });
+
+    function drawChart(emotion_data) {
+        var datasets = [],
+            labels = [],
+            i = 1;
+
+        emotion_names.forEach((emotion) => {
+            labels.push(i);
+            datasets.push({
+                data: emotion_data[emotion],
+                label: emotion,
+                borderColor: color_array[i],
+                fill: false
+            });
+            i += 1;
+        });
+
+        console.log(datasets);
+        console.log(labels);
+
+        new Chart(document.getElementById("chart_div"), {
+          type: 'line',
+          /*
+          data: {
+            labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+            datasets: [{ 
+                data: [86,114,106,106,107,111,133,221,783,2478],
+                label: "Africa",
+                borderColor: "#3e95cd",
+                fill: false
+              }, { 
+                data: [282,350,411,502,635,809,947,1402,3700,5267],
+                label: "Asia",
+                borderColor: "#8e5ea2",
+                fill: false
+              }, { 
+                data: [168,170,178,190,203,276,408,547,675,734],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+              }, { 
+                data: [40,20,10,16,24,38,74,167,508,784],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+              }, { 
+                data: [6,3,2,2,7,26,82,172,312,433],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+              }
+            ]
+          }*/
+          data: {
+            labels: labels,
+            datasets: datasets
+          }
+        });
+    };
 
     emotion_classifier.init(emotionModel);
 
@@ -41,16 +101,18 @@ window.onload = () => {
                     var current_parameters = ctrack.getCurrentParameters(),
                         emotions = emotion_classifier.meanPredict(current_parameters);
                     
-                    // console.log(emotions);
                     if (emotions) {
                         emotions.forEach(function (t) {
                             var emotion = t['emotion'];
                             emotion_data[emotion].push(t['value']);
                         });
                     }
-                    console.log(emotion_data);
                 }
             }, 500);
+
+            setInterval(() => {
+                drawChart(emotion_data);
+            }, 2000);
         }
     ).catch(
         (err) => {
